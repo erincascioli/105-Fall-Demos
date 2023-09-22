e68a0e14aa578c127680b65b02c6da6e695cb85a
erincascioli <edcigm@rit.edu>
2023-09-22 09:54:30 -0400

    Pushing new project to GitHub

--- IfStatements_Sec6/Program.cs
+++ IfStatements_Sec6/Program.cs
@@ -9,7 +9,26 @@ namespace IfStatements_Sec6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variable Block
+            int largeNumber = 20000;
+            double smallNumber = 20000;
+            bool isSnowingYet = false;
+            string food = "oatmeal";
+
+            //Console.WriteLine(largeNumber > smallNumber);
+            //Console.WriteLine(food > smallNumber);    // ERROR
+
+            //bool valuesAreEqual = (largeNumber == smallNumber);
+            //Console.WriteLine(valuesAreEqual);
+
+            // Conditional Statements
+            if(largeNumber == smallNumber)
+            {
+                Console.WriteLine("The values are the same!");
+            }
+
+            Console.WriteLine("Thanks for playing.");
+
         }
     }
 }
