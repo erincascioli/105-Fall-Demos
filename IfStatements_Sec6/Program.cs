3c50f7b470aa4d6f0a09fb5f289e2d038e58af7d
erincascioli <edcigm@rit.edu>
2023-09-22 09:16:25 -0400

    Initial commit of project to GitHub
    No code work has been done yet.

---
+++ IfStatements_Sec6/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Erin Cascioli
+// 9/22/23
+// Demo: If Statements
+
+namespace IfStatements_Sec6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
