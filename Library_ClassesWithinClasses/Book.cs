06e8f677082f93c7776c10311c0ccf57bb4cd072
erincascioli <edcigm@rit.edu>
2023-11-10 11:53:11 -0500

    Pushing Library demo for class work

---
+++ Library_ClassesWithinClasses/Book.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_ClassesWithinClasses
+{
+    internal class Book
+    {
+        // Fields
+        private string title;
+        private string author;
+        private int numberPages;
+        private int bookmarkedPage;
+        private int readThrough;
+
+        /// <summary>
+        ///  ???
+        /// </summary>
+        /// <param name="title">Title of the book</param>
+        /// <param name="author">Author of the book</param>
+        /// <param name="pages">Number of pages in the book</param>
+        public Book(string title, string author, int pages)
+        {
+            // These come from the params
+            this.title = title;
+            this.author = author;
+            this.numberPages = pages;
+
+            // Default values for these 2 fields only
+            bookmarkedPage = 0;
+            readThrough = 0;
+        }
+
+
+        /// <summary>
+        /// ???
+        /// </summary>
+        /// <param name="title">Title of the book</param>
+        /// <param name="author">Author of the book</param>
+        /// <param name="pages">Number of pages in the book</param>
+        /// <param name="pagesRead">
+        ///   Number of pages the book's owner has read</param>
+        /// <param name="readThrough">
+        ///   How many times an owner has fully read the book</param>
+        public Book(
+            string title,
+            string author,
+            int pages,
+            int pagesRead,
+            int readThrough)
+        {
+            this.title = title;
+            this.author = author;
+            this.numberPages = pages;
+            this.bookmarkedPage = pagesRead;
+            this.readThrough = readThrough;
+        }
+
+
+        /// <summary>
+        /// ???
+        /// </summary>
+        public Book()
+        {
+            title = "Book title";
+            author = "Book author";
+            numberPages = 0;
+            bookmarkedPage = 0;
+            readThrough = 0;
+        }
+
+
+        // Methods
+
+        /// <summary>
+        /// Prints the data for this book.
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine("Book:");
+            Console.WriteLine("  Book title: {0}", title);
+            Console.WriteLine("  Book author: {0}", author);
+            Console.WriteLine("  Pages: {0}", numberPages);
+            Console.WriteLine("  Bookmark is on page {0}", bookmarkedPage);
+            Console.WriteLine("  I've fully read this book {0} times.", readThrough);
+        }
+
+        /// <summary>
+        /// Simulates the reading of a book by x number of pages.
+        /// If a user exceeds the number of pages read, the readThrough field increases.
+        /// </summary>
+        /// <param name="pagesToRead">Number of pages someone is planning 
+        /// on reading. Must be positive.</param>
+        public void ReadBook(int pagesToRead)
+        {
+            if (pagesToRead > 0)
+            {
+                bookmarkedPage += pagesToRead;
+            }
+
+            // Pages read exceeds the total number of pages, meaning
+            //  the user has fully read through the book.
+            if (bookmarkedPage >= numberPages)
+            {
+                bookmarkedPage = 0;
+                readThrough++;
+            }
+        }
+    }
+}
---
+++ Library_ClassesWithinClasses/Library.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_ClassesWithinClasses
+{
+    internal class Library
+    {
+        // Fields
+        // Declare the array here
+        private Book[] myBooks;
+
+
+        // Constructor
+        /// <summary>
+        /// Instantiates a Library that contains 3 Books.
+        /// </summary>
+        public Library()
+        {
+            // Initialize the array here
+            // DON'T do this (it re-declares the array):
+            //Book[] myBooks = new Book[3];
+
+            // Do THIS:  (initializes the array field)
+            myBooks = new Book[5];
+
+            // Direct instantiation into the array
+            myBooks[0] = new Book();
+            myBooks[1] = new Book("How to be a pest", "Dash the cat", 1);
+            myBooks[2] = new Book("I LOVE candy!", "Zoe", 2);
+            myBooks[3] = new Book("When your professor talks too much...", "Erin Cascioli", 627);
+            myBooks[4] = new Book("Spells", "Winifred Sanderson", 1000, 15, 0);
+
+            // OR you could instantiate via variables
+            //Book defaultBook = new Book();
+            //myBooks[0] = defaultBook;
+            //Book dashBook = new Book("How to be a pest", "Dash the cat", 1);
+            //myBooks[1] = dashBook;
+            //Book zoeBook = new Book("I LOVE candy!", "Zoe", 2);
+            //myBooks[2] = zoeBook;
+            //Book erinBook = new Book("When your professor talks way too much...", "Erin Cascioli", 627);
+            //myBooks[3] = erinBook;
+            //Book hocusPocus = new Book("Spells", "Winifred Sanderson", 1000, 15, 0);
+            //myBooks[4] = hocusPocus;
+        }
+
+
+        /// <summary>
+        /// Prints the number of books in the library.
+        /// </summary>
+        public void NumberOfBooksIPurchased()
+        {
+            Console.WriteLine("I own {0} books.", myBooks.Length);
+        }
+
+
+        /// <summary>
+        /// Prints information about every Book in the library.
+        /// </summary>
+        public void PrintLibrary()
+        {
+            Console.WriteLine("These are the books in my library: ");
+
+            // Iterate through every book on my shelf
+            //   and print information about it
+            for (int i = 0; i < myBooks.Length; i++)
+            {
+                myBooks[i].Print();
+            }
+        }
+
+
+        /// <summary>
+        /// Searches for a titled book in the library.
+        /// </summary>
+        /// <param name="titleFromParam">Title of the desired book.</param>
+        /// <returns>True if the book is in the library, false otherwise.</returns>
+        public bool SearchForBook(string titleFromParam)
+        {
+            // PSEUDOCODE:
+            // Iterate through all books in the array
+            //     Does the book title match the parameter?
+            //     Yes --> true
+            // After the search, did not find book --> false
+            // Return true or false
+
+            // Start by assuming the book is NOT there.
+            bool inLibrary = false;
+
+            // Search through all books in the library
+            for (int i = 0; i < myBooks.Length; i++)
+            {
+                // Wait...  I can't do this because title is private...
+                // Check is the title matches this method's parameter
+                /*
+                if (titleFromParam == myBooks[i].title)
+                {
+                    inLibrary = true;
+                }
+                */
+            }
+
+            // Return the result.
+            return inLibrary;
+        }
+    }
+}
