2841ad05de3d1e3f5e6d2bb2751168f2876d3360
erincascioli <edcigm@rit.edu>
2023-11-10 06:44:19 -0500

    pushing List demo to repo

---
+++ Library/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Card
+    {
+        private int value;
+        private string suit;
+
+        public Card(int value, string suit)
+        {
+            this.value = value;
+            this.suit = suit;
+        }
+    }
+}
---
+++ Library/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Library
+{
+    internal class Program
+    {
+        // --------------------------------------------------------------------
+        // Fields
+        // --------------------------------------------------------------------
+        private Random generator;
+        //private Card[] cardCollection;        // Comment out! Don't delete!
+        private List<Card> cardCollection;
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            
+
+        }
+    }
+}
