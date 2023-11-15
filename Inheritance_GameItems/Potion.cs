2d1e575e340799b062495467ccb96915da9c7e6e
erincascioli <edcigm@rit.edu>
2023-11-15 11:47:28 -0500

    Pushing inheritance demo

---
+++ Inheritance_GameItems/Potion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_GameItems
+{
+    /// <summary>
+    /// Child class of GameItem.
+    /// Represents a liquid potion that can be drank.
+    /// </summary>
+    internal class Potion : GameItem
+    {
+        // Fields that are unique to Potion        
+        private string effect;
+        private int uses;
+
+        
+        /// <summary>
+        /// Constructor for a Potion object, where all Potions are named "potion"
+        /// and have the same default information for costs and weight.
+        /// </summary>
+        /// <param name="effect">The effect upon the player</param>
+        /// <param name="uses">How many doses exist</param>
+        public Potion(string effect, int uses) : 
+            base("potion", 25, 2, 0.5)
+        {
+            this.effect = effect;
+            this.uses = uses;
+        }
+
+
+        /// <summary>
+        /// Constructor for a Potion object, where all Potions have custom names,
+        /// and a unique purchase cost. Sell cost and weight are default.
+        /// </summary>
+        /// <param name="name">Name of the potion</param>
+        /// <param name="purchaseCost">How much the potion costs when purchasing</param>
+        /// <param name="effect">The effect upon the player</param>
+        /// <param name="uses">How many doses exist</param>
+        public Potion(string name, double purchaseCost, string effect, int uses) : 
+            base(name, purchaseCost, 2, 0.5)
+        {
+            this.effect = effect;
+            this.uses = uses;
+        }
+
+
+        /// <summary>
+        /// Use the potion by drinking a dose.
+        /// </summary>
+        public void Drink()
+        {
+            // As long as enough doses are left, drink the potion!
+            if(uses > 0)
+            {
+                Console.WriteLine($"You drink the {name} potion, which has the effect of {effect}.");
+                uses--;
+            }
+            else
+            {
+                Console.WriteLine("Not enough potion left to use it.");
+            }
+        }
+
+    }
+}
---
+++ Inheritance_GameItems/Weapon.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_GameItems
+{
+    /// <summary>
+    /// Child class of GameItem.
+    /// Represents any weapon.
+    /// </summary>
+    internal class Weapon : GameItem
+    {
+        // Fields that are unique to Weapon
+        private double damageDealt;
+        private double durability;
+        private double maxDurability;
+        private string durabilityCategory;
+        private double deteriorationPercentage;
+        private bool isRepairable;
+
+
+        /// <summary>
+        /// Constructor for the Weapon class
+        /// </summary>
+        /// <param name="damage">Amount of damage this weapon deals (when at full durability)</param>
+        /// <param name="maxDurability">Full durability</param>
+        /// <param name="durabilityCategory">Is this weapon fragile, sturdy, or hardy?</param>
+        /// <param name="name">Name of the weapon</param>
+        /// <param name="purchaseCost">Cost to purchase</param>
+        /// <param name="sellCost">Cost to sell</param>
+        /// <param name="weight">Weight of the item</param>
+        public Weapon(
+            double damage, double maxDurability, string durabilityCategory, bool repairable,    // Unique to Weapon
+            string name, double purchaseCost, double sellCost, double weight) :                 // Sent to parent
+            base(name, purchaseCost, sellCost, weight)                                          // Call parent constructor
+        {
+            this.damageDealt = damage;
+            this.maxDurability = maxDurability;
+            this.durability = maxDurability;
+            this.durabilityCategory = durabilityCategory;
+            this.isRepairable = repairable;
+
+            // Determing fragility based on the durability category
+            switch(this.durabilityCategory)
+            {
+                case "fragile":
+                    deteriorationPercentage = 0.6;
+                    break;
+                case "sturdy":
+                    deteriorationPercentage = 0.2;
+                    break;
+                case "hardy":
+                    deteriorationPercentage = 0.05;
+                    break;
+            }
+        }
+
+
+        /// <summary>
+        /// Repair the weapon back to its maximum value.
+        /// </summary>
+        public void Repair()
+        {
+            if(isRepairable)
+            {
+                durability = maxDurability;
+                Console.WriteLine("Weapon is repaired!");
+            }
+            else
+            {
+                Console.WriteLine("Weapon cannot be repaired!");
+            }
+        }
+
+
+        /// <summary>
+        /// Attack with this weapon
+        /// </summary>
+        /// <returns>Hit points with which to damage other players</returns>
+        public double Attack()
+        {
+            double attackDamage = 0;
+
+            // Damage is reduced to 50% when durability is between 1% and 50%
+            // The weapon takes a durability hit
+            if (durability <= (maxDurability * 0.5))
+            {
+                attackDamage = damageDealt * 0.5;
+            }
+            // Full damage when > 50% durability
+            else if(durability > 0)
+            {
+                attackDamage = damageDealt;
+            }
+
+            // Weapon is reduced by the deterioration percentage of its max durability
+            durability -= maxDurability * deteriorationPercentage;
+
+            Console.WriteLine($"{name} attacks for {attackDamage}!");
+            return attackDamage;
+        }
+    }
+}
