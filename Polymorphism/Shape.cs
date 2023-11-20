ff60d6e51cc75ffdb5a7d644b0deae8d0328b453
erincascioli <edcigm@rit.edu>
2023-11-20 11:52:22 -0500

    Pushing polymorphism demo

---
+++ Polymorphism/Shape.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Shape
+    {
+        protected double area;
+        protected int numberSides;
+        protected ConsoleColor color;
+
+        public double Area
+        {
+            get { return area; }
+        }
+
+
+        /// <summary>
+        /// Constructs a default Shape object
+        /// </summary>
+        public Shape()
+        {
+            area = 0;
+            numberSides = 0;
+            color = ConsoleColor.DarkGreen;
+        }
+
+
+        /// <summary>
+        /// Calculates the area of a Shape.
+        /// </summary>
+        /// <returns>The area of the specified shape</returns>
+        public virtual double CalculateArea()
+        {
+            return 0.0;
+        }
+
+
+        /// <summary>
+        /// Prints information about the Shape.
+        /// </summary>
+        public virtual void PrintInformation()
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine("I am a SHAPE!");
+            Console.WriteLine("???");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        /// <summary>
+        /// Provides a string representation of the Shape object.
+        /// </summary>
+        /// <returns>String representation</returns>
+        public override string ToString()
+        {
+            string shapeInformation = $"I am a Shape object with area {area}";
+            
+            if (numberSides > 0)
+            {
+                shapeInformation += $", color {color}, and {numberSides} sides.";
+            }
+            else
+            {
+                shapeInformation += $" and no sides.";
+            }
+
+            return shapeInformation;
+        }
+    }
+}
---
+++ Polymorphism/Square.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Square : Shape
+    {
+        private double sideLength;
+
+
+        /// <summary>
+        /// Constructs a Square object
+        /// </summary>
+        /// <param name="sideLength">Length of one side</param>
+        /// <param name="color">Text color of visual representations</param>
+        public Square(double sideLength, ConsoleColor color) :
+            base()
+        {
+            this.numberSides = 4;
+            this.sideLength = sideLength;
+            this.color = color;
+            this.area = CalculateArea();
+        }
+
+
+        /// <summary>
+        /// Calculates the area of this square based on its length of side.
+        /// </summary>
+        /// <returns>Area of this square</returns>
+        public override double CalculateArea()
+        {
+            return sideLength * sideLength;
+        }
+
+
+        /// <summary>
+        /// Displays Square information in the Console window. 
+        /// </summary>
+        public override void PrintInformation()
+        {
+            // Print method overrides the parent's to use its own unique version
+            //   that works with a Circle object.
+            Console.ForegroundColor = color;
+            Console.WriteLine("I am a SQUARE!");
+            Console.WriteLine("OOOOOO");
+            Console.WriteLine("O    O");
+            Console.WriteLine("O    O");
+            Console.WriteLine("OOOOOO");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        /// <summary>
+        /// Provides a string representation of the Square object.
+        /// </summary>
+        /// <returns>String representation</returns>
+        public override string ToString()
+        {
+            // ToString method overrides the parent's to use its own unique version
+            //   that works with a Square object.
+            return $"I am a Square object with {numberSides} sides, a side length of {sideLength}, " +
+                $"area {area} and in {color} color.";
+        }
+    }
+}
