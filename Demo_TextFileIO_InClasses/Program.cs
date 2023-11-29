1452ccf88eaac68308b348e308e85ba6200afd5a
erincascioli <edcigm@rit.edu>
2023-11-29 06:29:09 -0500

    Pushing File IO Demos
    No serialization in these demos. Only reading and writing with try/catch/finally blocks.

---
+++ Demo_TextFileIO_InClasses/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TextFileIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read from the file myFile.txt:");
+            FileReader myFileReader = new FileReader("../../../MyFile.txt");
+
+            Console.WriteLine("\nWrite to the file myWrittenFile.txt:");
+            FileWriter myFileWriter = new FileWriter("../../../MyWrittenFile.txt");
+
+            Console.WriteLine("\nRead from the file myWrittenFile.txt:");
+            FileReader myFileReader2 = new FileReader("../../../MyWrittenFile.txt");
+        }
+    }
+}
